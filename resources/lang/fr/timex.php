7a6b70930f39f3ca843451d8fc6e61ff59ddf2c4
Jean-Vincent kassi <jeanvincent45@gmail.com>
2023-06-22 22:25:02 +0000

    Add french

---
+++ resources/lang/fr/timex.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'model' => [
+      'label' => 'Event',
+      'pluralLabel' => 'Events',
+    ],
+    'modal' => [
+        'submit' => 'Submit',
+        'cancel' => 'Cancel',
+        'delete' => 'Delete',
+        'edit' => 'Edit',
+    ],
+    'events' => [
+        'empty' => 'No upcoming :label'
+    ],
+    'event' => [
+      'subject' => 'Subject',
+      'body' => 'Body',
+      'category' => 'Category',
+      'allDay' => 'All day',
+      'start' => 'Start',
+      'end' => 'End',
+      'participants' => 'Participants',
+      'attachments' => 'Attachments',
+
+    ],
+    'event-list' => [
+        'author' => 'Author: :name',
+        'start' => 'Start: :start',
+        'end' => 'End: :end'
+    ],
+    'labels' => [
+      'navigation' => 'TiMEX',
+      'breadcrumbs' => 'TiMEX',
+      'title' => 'TiMEX',
+      'today' => 'Today',
+    ],
+];
